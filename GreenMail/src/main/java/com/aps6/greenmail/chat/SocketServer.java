5d99d791fd069016a234e68f2454c16871d7f759
Ruiran Oliveira <ruiranoliveirasantos@gmail.com>
2020-07-24 22:39:22 -0300

    Creating the socket server of the application

---
+++ GreenMail/src/main/java/com/aps6/greenmail/chat/SocketServer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aps6.greenmail.chat;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author ruiran
+ */
+public class SocketServer {
+    
+    public static void main(String[] args) {
+        
+        ServerSocket server = null;
+        
+        try {
+            server = new ServerSocket(9999);
+            System.out.println("server running...");
+            
+            while (true) {
+                Socket client = server.accept();
+                new ClientHandler(client);
+            }
+            
+        } catch (IOException ex) {
+            try {
+                if (server != null)
+                    server.close();
+            } catch (IOException ex1) {}
+            System.err.println("Server closed.");
+        }
+    }
+}
